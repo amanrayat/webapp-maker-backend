b767f3396c3c426c0c61b8613be0727176c9abb4
amanrayat <amanrayat32@gmail.com>
2019-11-22 14:18:40 -0500

    Adding mongo Heroku

--- db/database.js
+++ db/database.js
@@ -1,7 +1,7 @@
 module.exports = function () {
     const mongoose = require('mongoose');
-    const databaseName = 'webappmakerdb';
-    let connectionString = 'mongodb://127.0.0.1/';
+    const databaseName = 'heroku_63z8ldhk';
+    let connectionString = 'mongodb://webappmaker:webappmaker1@ds153766.mlab.com:53766/';
     connectionString += databaseName;
     mongoose.connect(connectionString, {useNewUrlParser: true}).then((result) => {
         console.log(result)
