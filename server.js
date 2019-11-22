d5f828e41f36d998135250821d9e59909154df3b
amanrayat <amanrayat32@gmail.com>
2019-11-22 14:59:54 -0500

    Fixing URL

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(session({
     rolling: true
 }));
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://webapp-maker-neu.herokuapp.com");
+    res.header("Access-Control-Allow-Origin", "https://webapp-maker-frontend.herokuapp.com");
     res.set('Access-Control-Allow-Credentials', 'true');
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
